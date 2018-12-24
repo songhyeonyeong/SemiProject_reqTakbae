ab242226c7524e486524530f628b53e0a8ec4ae4
songhyeonyeong <42529170+songhyeonyeong@users.noreply.github.com>
2018-12-24 14:30:29 +0900

    ㅇㄴ

--- src/com/kh/jinkuk/border/announcment/cotroller/announmentServlet.java
+++ src/com/kh/jinkuk/border/announcment/cotroller/announmentServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class TestServlet
  */
 @WebServlet("/TestServlet")
-public class announmentServlet extends HttpServlet {
+public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
