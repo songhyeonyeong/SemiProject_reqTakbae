8762f9eda010bf5910f4d77dbb5291bc08d76193
한민규 <@DESKTOP-L3VM75J>
2018-12-26 15:08:53 +0900

    mingyu_20181226_1

--- src/com/kh/jinkuk/change/controller/ChangeServlet.java
+++ src/com/kh/jinkuk/change/controller/ChangeServlet.java
@@ -26,8 +26,14 @@ public class ChangeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String amount = request.getParameter("amount");
+		String approval = request.getParameter("approval");
+		
+		System.out.println(amount);
+		System.out.println(approval);
+		
+		
+		
 	}
 
 	/**
