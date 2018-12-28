f8122d2aab7d23cfca5ce0c5acd865bc5005cdb3
한민규 <@DESKTOP-L3VM75J>
2018-12-28 15:07:48 +0900

    mingyu_20181228_3

--- src/com/kh/jinkuk/cybermoney_breakdown/model/vo/Cybermoney_breakdown.java
+++ src/com/kh/jinkuk/cybermoney_breakdown/model/vo/Cybermoney_breakdown.java
@@ -1,5 +1,80 @@
 package com.kh.jinkuk.cybermoney_breakdown.model.vo;
 
-public class Cybermoney_breakdown {
+import java.sql.Date;
 
+public class Cybermoney_breakdown {
+	private int r_num;
+	private String cm_div;
+	private Date cm_date;
+	private int cm_use;
+	private String cm_note;
+	private int c_money;
+	
+	public Cybermoney_breakdown() {}
+	
+	public Cybermoney_breakdown(int r_num, String cm_div, Date cm_date, int cm_use, String cm_note, int c_money) {
+		super();
+		this.r_num = r_num;
+		this.cm_div = cm_div;
+		this.cm_date = cm_date;
+		this.cm_use = cm_use;
+		this.cm_note = cm_note;
+		this.c_money = c_money;
+	}
+
+	public int getR_num() {
+		return r_num;
+	}
+
+	public void setR_num(int r_num) {
+		this.r_num = r_num;
+	}
+
+	public String getCm_div() {
+		return cm_div;
+	}
+
+	public void setCm_div(String cm_div) {
+		this.cm_div = cm_div;
+	}
+
+	public Date getCm_date() {
+		return cm_date;
+	}
+
+	public void setCm_date(Date cm_date) {
+		this.cm_date = cm_date;
+	}
+
+	public int getCm_use() {
+		return cm_use;
+	}
+
+	public void setCm_use(int cm_use) {
+		this.cm_use = cm_use;
+	}
+
+	public String getCm_note() {
+		return cm_note;
+	}
+
+	public void setCm_note(String cm_note) {
+		this.cm_note = cm_note;
+	}
+
+	public int getC_money() {
+		return c_money;
+	}
+
+	public void setC_money(int c_money) {
+		this.c_money = c_money;
+	}
+
+	@Override
+	public String toString() {
+		return "Cybermoney_breakdown [r_num=" + r_num + ", cm_div=" + cm_div + ", cm_date=" + cm_date + ", cm_use="
+				+ cm_use + ", cm_note=" + cm_note + ", c_money=" + c_money + "]";
+	}
+	
+	
 }
