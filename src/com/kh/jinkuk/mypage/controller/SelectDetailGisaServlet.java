8b50253897371fd0e1f02071991afd050c5a0ee6
songhyeonyeong <songhyeonyeong@DESKTOP-E3OPP6Q>
2019-01-02 09:23:04 +0900

    0102_01

--- src/com/kh/jinkuk/mypage/controller/SelectDetailGisaServlet.java
+++ src/com/kh/jinkuk/mypage/controller/SelectDetailGisaServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.kh.jinkuk.mypage.model.service.MypageService;
 import com.kh.jinkuk.mypage.model.vo.SelectReqGisa;
 
@@ -31,6 +32,18 @@ public class SelectDetailGisaServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int num =Integer.parseInt(request.getParameter("num"));
 		SelectReqGisa srg= new MypageService().SelectDetailGisa(num);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		new Gson().toJson(srg, response.getWriter());
+		
+	/*	if(srg!=null) {
+			request.setAttribute("detailGisa",srg);
+			request.getRequestDispatcher("/views/mypage/deliDetailPop.jsp").forward(request, response);
+			
+			
+		}*/
+		
 		
 		
 		
